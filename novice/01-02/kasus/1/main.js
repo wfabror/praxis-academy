045b620650445e1883d43a09c4bc7d3766a0f3e0
abror <wf.abror@gmail.com>
2019-09-03 16:34:20 +0700

    Pekan 1 Hari 2

---
+++ novice/01-02/kasus/1/main.js
@@ -0,0 +1,14 @@
+let angka1, angka2;
+angka1 = prompt("Masukkan angka pertama", "0");
+angka2 = prompt("Masukkan angka kedua", "0");
+
+
+function perbandingan() {
+    if (parseInt(angka1) > parseInt(angka2)) {
+        console.log("Angka besar adalah " + angka1);
+    } else if (parseInt(angka2) > parseInt(angka1)) {
+        console.log("Angka besar adalah " + angka2);
+    } else {
+        console.log("Angkanya sama");
+    }
+}
