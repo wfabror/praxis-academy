4dba51d227873a8d1fbfb3375aba306728fb50f2
abror <wf.abror@gmail.com>
2019-09-05 09:36:22 +0700

    Hari 2 Pekan 1

--- novice/01-02/kasus/6/main.js
+++ novice/01-02/kasus/6/main.js
@@ -1,10 +1,28 @@
-var David = 80;
-Vinoth = 77;
-Divya = 88;
-Ishitha = 95;
-Thamas = 68;
+var murid = [
+    ['David', 80],
+    ['Vinoth', 77],
+    ['Divya', 88],
+    ['Ishitha', 95],
+    ['Thamas', 68]
+];
 
-avg = ((David + Vinoth + Divya + Ishitha + Thamas) / 5);
-console.log("Average marks " + avg);
+var avrgMarks = 0;
 
-// /if
+for (var i = 0; i < murid.length; i++) {
+    avrgMarks += murid[i][1];
+    var avg = (avrgMarks / murid.length);
+}
+
+console.log("Average grade " + (avrgMarks) / murid.length);
+
+if (avg < 60) {
+    console.log("Grade : F");
+} else if (avg < 70) {
+    console.log("Grade : D");
+} else if (avg < 80) {
+    console.log("Grade : C")
+} else if (avg < 90) {
+    console.log("Grade : B");
+} else if (avg < 100) {
+    console.log("Grade : A ")
+}
