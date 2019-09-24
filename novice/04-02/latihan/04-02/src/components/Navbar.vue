80cc9e01845a370eecc5f1c25d9d4cced97e8355
abror <wf.abror@gmail.com>
2019-09-24 16:34:31 +0700

    Latihan Vue

---
+++ novice/04-02/latihan/04-02/src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div>
+        <b-nav pills>
+            <b-nav-item active>Active</b-nav-item>
+            <b-nav-item >Link</b-nav-item>
+            <b-nav-item>Another Link</b-nav-item>
+            <b-nav-item disabled>Disabled</b-nav-item>
+        </b-nav>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods:{
+       isActive: function(event){
+           if(event){
+               active
+           }
+       }
+    }
+}
+</script>
+
---
+++ novice/04-02/latihan/04-02/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <div class="container-fluid">
+        <navbar/>
+    </div>
+    
+    <img alt="Vue logo" src="../assets/logo.png">
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    Navbar
+  }
+}
+</script>
